324a88705d28d6a05764af2ebaa28721103fb63c
TO <takahiro.ohori@gmail.com>
2024-02-12 14:14:12 +0900

    add: klines api call

---
+++ app/infrastructure/externalapi/klines.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from enum import Enum
+
+import requests
+
+
+class PriceType(Enum):
+    ASK = "ASK"
+    BID = "BID"
+
+
+class Interval(Enum):
+    ONE_MINUTE = "1min"
+    FIVE_MINUTE = "5min"
+    TEN_MINUTE = "10min"
+    FIFTEEN_MINUTE = "15min"
+    THIRTY_MINUTE = "30min"
+    ONE_HOUR = "1hour"
+    FOUR_HOUR = "4hour"
+    EIGHT_HOUR = "8hour"
+    TWELVE_HOUR = "12hour"
+    ONE_DAY = "1day"
+    ONE_WEEK = "1week"
+    ONE_MONTH = "1month"
+
+
+GMO_PUBLIC_API_ENDPOINT = "https://forex-api.coin.z.com/public"
+
+
+@dataclass
+class OHLCV:
+    openTime: int
+    open: float
+    high: float
+    low: float
+    close: float
+
+
+@dataclass
+class Klines:
+    status: int
+    data: list[OHLCV]
+    responsetime: str
+
+
+def get_klines(symbol: str, price_type: PriceType, interval: Interval, date: str) -> Klines:
+    url = f"{GMO_PUBLIC_API_ENDPOINT}/v1/klines?symbol={symbol}&priceType={price_type}&interval={interval}&date={date}"
+
+    response = requests.get(url)
+    return response.json()
